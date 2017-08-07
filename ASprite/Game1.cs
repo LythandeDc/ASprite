21ae38bfad6792113d2b0eb0e0be85fcd7f541f5
Lythande <lythande_gamespeed@yahoo.fr>
2017-08-07 18:44:45 +0200

    First commit

---
+++ ASprite/Game1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace ASprite
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Sprite _sprite1;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var texture = Content.Load<Texture2D>("dead");
+            _sprite1 = new Sprite(texture);
+            _sprite1.Position = new Vector2(50, 50);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _sprite1.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+            _sprite1.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ASprite/Sprite.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASprite
+{
+    public class Sprite
+    {
+        private Texture2D _texture; //  sprite texture 
+        public Vector2 Position; //  sprite position on screen
+        public float Speed = 2f; // sprite speed
+
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public void Update()
+        {
+            // Left
+            if(Keyboard.GetState().IsKeyDown(Keys.Q) ||
+               Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                Position.X -= Speed;
+            }
+
+            // Right
+            if (Keyboard.GetState().IsKeyDown(Keys.D) ||
+               Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Position.X += Speed;
+            }
+
+            // Up
+            if (Keyboard.GetState().IsKeyDown(Keys.Z) ||
+               Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                Position.Y -= Speed;
+            }
+
+            // Down
+            if (Keyboard.GetState().IsKeyDown(Keys.S) ||
+               Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                Position.Y += Speed;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Adds a sprite to a batch of sprites for rendering using the specified texture, 
+            // position, source rectangle, color, rotation, origin, scale, effects and layer.
+            spriteBatch.Draw(_texture, Position, Color.White);
+        }
+    }
+}
